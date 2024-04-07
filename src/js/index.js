d94de81d051ba5fb732470214d97c63ca5fb8756
Ricardo Gabriel <112662253+ricglb@users.noreply.github.com>
2024-04-07 18:57:15 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -18,13 +18,32 @@
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
-botaoMostrarProjetos.addEventListener('click', () => {
-    // Passo 3
-    mostrarMaisProjetos();
 
-    //Objetivo 2
+const botaoEsconderProjetos = document.querySelector('.btn-esconder-projetos');
+
+botaoEsconderProjetos.classList.add("remover");
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
     esconderBotao();
+    botaoEsconderProjetos.classList.remove("remover");
 });
+
+botaoEsconderProjetos.addEventListener('click', () => {
+    esconderProjetos();
+    mostrarBotao();
+    botaoEsconderProjetos.classList.add("remover");
+})
+
+function mostrarBotao() {
+    botaoMostrarProjetos.classList.remove("remover");
+}
+
+function esconderProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
 
 function esconderBotao() {
     botaoMostrarProjetos.classList.add("remover");
@@ -35,4 +54,3 @@ function mostrarMaisProjetos() {
         projetoInativo.classList.add('ativo');
     });
 }
-
